0ac7207eaf67983b9ef6fef0d09fa5b065906dd1
Jorge Bau Gracia <jbaugracia@gmail.com>
2024-12-08 20:32:42 +0100

    Now using the sandbox extension for tests

--- src/test/java/unxutils/coreutils/TestListDirectoryCommand.java
+++ src/test/java/unxutils/coreutils/TestListDirectoryCommand.java
@@ -9,9 +9,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.apache.commons.lang.RandomStringUtils.random;
 import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
-import static org.apache.commons.lang.RandomStringUtils.randomAscii;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
